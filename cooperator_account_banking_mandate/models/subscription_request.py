888cfb60ae5d2038a36f97edae7f569d583ee8d7
daniquilez <dani.quilez@gmail.com>
2025-03-05 11:16:23 +0100

    [IMP] ✨ Better mandate creation workflow

--- cooperator_account_banking_mandate/models/subscription_request.py
+++ cooperator_account_banking_mandate/models/subscription_request.py
@@ -21,47 +21,54 @@ class SubscriptionRequest(models.Model):
     )
 
     def create_invoice(self, partner):
-        if self.mandate_required and not self.mandate_id:
-            self.mandate_id = self.create_mandate()
+        if self.mandate_required:
+            existing_rpb = self._get_existing_rpb()
+            if not existing_rpb:
+                existing_rpb = self._create_bank()
+            if existing_rpb and not existing_rpb.mandate_ids:
+                self._validate_mandate_creation()
+                # self.mandate_id = self._create_mandate(existing_rpb)
         return super().create_invoice(partner)
 
-    def create_mandate(self):
+
+    def _validate_mandate_creation(self):
         if not self.partner_id:
             raise ValidationError(_("Must assign a valid cooperator."))
         if not self.mandate_approved:
             raise ValidationError(_("Must check the mandate creation."))
-        return self.env["account.banking.mandate"].create(self.get_mandate_values())
+        if not self.iban:
+            raise ValidationError(_("Must assign a valid iban."))
+
+    def _create_mandate(self, rpb):
+        return self.env["account.banking.mandate"].create({
+            "format": "sepa",
+            "type": "recurrent",
+            "state": "valid",
+            "signature_date": self.date,
+            "partner_bank_id": rpb.id,
+            "partner_id": self.partner_id.id,
+            "company_id": self.company_id.id,
+        })
 
     def get_invoice_vals(self, partner):
         vals = super().get_invoice_vals(partner)
         vals["mandate_id"] = self.mandate_id.id
         return vals
 
-    def get_bank(self):
-        if not self.iban:
-            raise ValidationError(_("Must assign a valid iban."))
-        bank_id = self.partner_id.bank_ids.filtered(lambda x: x.acc_number)
-        # TODO normalize iban
-        if bank_id:
-            return bank_id
+    def _get_existing_rpb(self):
+        return self.partner_id.bank_ids.filtered(lambda rpb: rpb.acc_number == self.iban and rpb.company_id == self.company_id.id)
+
+    def _create_bank(self):
         return self.env["res.partner.bank"].create(
             {
                 "partner_id": self.partner_id.id,
                 "acc_number": self.iban,
+                "company_id": self.company_id.id
             }
         )
 
     def get_mandate_values(self):
-        bank_id = self.get_bank()
-        return {
-            "format": "sepa",
-            "type": "recurrent",
-            "state": "valid",
-            "signature_date": self.date,
-            "partner_bank_id": bank_id.id,
-            "partner_id": self.partner_id.id,
-            "company_id": self.company_id.id,
-        }
+        return 
 
     @api.onchange("partner_id")
     def onchange_partner(self):
