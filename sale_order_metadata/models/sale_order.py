2914f4d87dece8b6be908d7dea538cc8f9ecd06c
daniquilez <dani.quilez@gmail.com>
2025-03-10 15:54:43 +0100

    [IMP] ✨ Add util method to retrieve metadata value

--- sale_order_metadata/models/sale_order.py
+++ sale_order_metadata/models/sale_order.py
@@ -7,3 +7,8 @@ class SaleOrder(models.Model):
 
     metadata_line_ids = fields.One2many(
         'sale.order.metadata.line', 'order_id', string=_("Metadata"))
+
+    def get_metadata_value(self, key):
+        return self.metadata_line_ids.filtered(
+            lambda meta: meta.key == key
+        ).value
