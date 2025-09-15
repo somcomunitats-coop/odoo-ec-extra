50767d4b17c81590599f7e96bdd6e5c06f241a7c
Álvaro García <alvarogruiz8@gmail.com>
2025-09-15 09:41:54 +0200

    [REF] ♻️  Up version of manifest
    Author: Alvaro Garcia <alvaro.garcia@somit.coop>

--- cooperator_account_banking_mandate/__manifest__.py
+++ cooperator_account_banking_mandate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Cooperator Account Banking Mandate",
-    "version": "16.0.0.2.1",
+    "version": "16.0.0.2.2",
     "license": "AGPL-3",
     "summary": """
         This module adds mandate selection to cooperator subscription request.""",
